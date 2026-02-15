7c3d9d1dfa37391e169595a2804728b8c9b93378
davidimbachi <davidimbachi2111@hotmail.com>
2026-02-14 19:07:09 -0500

    arreglos

--- backend/routes/usuariosRoutes.js
+++ backend/routes/usuariosRoutes.js
@@ -1,4 +1,4 @@
-//import express from "express";
+import express from "express";
 const router = express.Router();
 
 import {registrar,login,confirmarToken,olvidePassword,nuevoPassword,perfil} from "../controllers/usuarioController.js";
@@ -7,14 +7,19 @@ import checkAuth from "../middleware/checkAuth.js";
 
 // Crear usuario
 router.post("/", registrar);
+
 // Login
 router.post("/login", login);
+
 // Confirmar cuenta con token
 router.get("/confirmar/:token", confirmarToken);
+
 // Olvidé password
 router.post("/olvide-password", olvidePassword);
+
 // Reset password con token
 router.post("/olvide-password/:token", nuevoPassword);
+
 // Perfil protegido
 router.get("/perfil", checkAuth, perfil);
 
