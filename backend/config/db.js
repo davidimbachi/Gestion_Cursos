1004e02b5cda2714a829c9ce143ff968f98602cf
2005wendy <wendycordoba178@gmail.com>
2026-02-16 08:22:58 -0500

    corregido link mongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 // import 'dotenv/config'
 const conectarDB = async () => {
     try {
-        const connection =await mongoose.connect('mongodb+srv://pacho:root@gestioncursos.wjahq5m.mongodb.net/?appName=GestionCursos');
+        const connection =await mongoose.connect('mongodb+srv://cursos_complementarios:root@cursoscomplentarios.qxim8nn.mongodb.net/');
             // useNewUrlParser : true,
             // useUnifiestopology:true
         
