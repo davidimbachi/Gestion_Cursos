05a1d91279709e3cd6a95591042ecfc56bcae4e0
2005wendy <wendycordoba178@gmail.com>
2026-02-16 10:11:24 -0500

    router

--- backend/routes/ofertas.routes.js
+++ backend/routes/ofertas.routes.js
@@ -1,24 +1,16 @@
 //Encargado de crear y administrar ofertas:
-
 import express from "express";
 const router = express.Router();
 
 // Importamos funciones del controlador de ofertas
-import { crearOferta, listarOfertas, actualizarOferta, eliminarOferta } from "../controllers/ofertaController.js";
+import { crearOferta, listarOfertas, actualizarOferta } from "../controllers/ofertaController.js";
 
 // Middleware para proteger rutas privadas
 import checkAuth from "../middleware/checkAuth.js";
 
-// Crear nueva oferta (requiere autenticación)
-router.post("/", checkAuth, crearOferta);
+router.post("/", checkAuth, crearOferta); // Crear nueva oferta (requiere autenticación)
+router.get("/", listarOfertas);// Listar todas las ofertas (público)
+router.put("/:id", checkAuth, actualizarOferta);// Actualizar una oferta por ID (requiere autenticación)
 
-// Listar todas las ofertas (público)
-router.get("/", listarOfertas);
-
-// Actualizar una oferta por ID (requiere autenticación)
-router.put("/:id", checkAuth, actualizarOferta);
-
-// Eliminar una oferta por ID (requiere autenticación)
-router.delete("/:id", checkAuth, eliminarOferta);
 
 export default router;
